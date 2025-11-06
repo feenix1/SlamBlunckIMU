2e881910b6ce542bfedb4254a942e48f85b26402
Matthew Dinh <72724975+feenix1@users.noreply.github.com>
2025-11-06 14:51:08 -0800

    add bno055 library

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,22 @@
 #include <Arduino.h>
+#include <Wire.h>
+#include <7Semi_BNO055.h>
+
+BNO055_7Semi imu = BNO055_7Semi();
+
+
 
 // put function declarations here:
 int myFunction(int, int);
 
 void setup() {
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  imu.begin();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  printf("hello world");
 }
 
 // put function definitions here:
